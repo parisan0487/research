45de6333bebb7703bbcb6cb29035dcab13f3f3a2
parisan <parisan0487@gmail.com>
2025-05-23 21:49:07 +0330

    new

--- src/component/layout/admin-panel/AddProductToDB.js
+++ src/component/layout/admin-panel/AddProductToDB.js
@@ -16,9 +16,9 @@ const AddProductsToDB = () => {
       categories: ["b1", "b2", "b3"],
       producter: "تهران",
       images: [
-        "/assets/img/product4.webp",
-        "/assets/img/product4.webp",
-        "/assets/img/product4.webp",
+        "/assets/img/handicrafts.png",
+        "/assets/img/handicrafts.png",
+        "/assets/img/handicrafts.png",
       ],
       variants: [{ color: "مشکی", size: "2xl", stock: 10 }],
     },
@@ -30,7 +30,7 @@ const AddProductsToDB = () => {
       description: "شکلات خوری چوبی معرق کاری",
       categories: ["b1", "b3", "b5"],
       producter: "تهران",
-      images: ["/assets/img/product4.webp"],
+      images: ["/assets/img/handicrafts.png"],
       variants: [{ color: "مشکی", size: "2xl", stock: 10 }],
     },
     {
@@ -41,7 +41,7 @@ const AddProductsToDB = () => {
       description: "شکلات خوری چوبی معرق کاری",
       categories: ["b4", "b5", "b3", "b1"],
       producter: "تهران",
-      images: ["/assets/img/product4.webp"],
+      images: ["/assets/img/handicrafts.png"],
       variants: [{ color: "مشکی", size: "", stock: 10 }],
     },
   ];
